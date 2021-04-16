3b89a62807026aea58a3d6f1241b2049e65e5243
henfy233 <619899573@qq.com>
2021-04-16 12:31:37 +0800

    Site updated: 2021-04-16 12:31:37

--- bundle.js
+++ bundle.js
@@ -77,22 +77,6 @@ CONFIG.prism&&window.Prism.highlightAll(),CONFIG.fancybox&&NexT.utils.wrapImageW
 // Define Motion Sequence & Bootstrap Motion.
 CONFIG.motion.enable&&NexT.motion.integrator.add(NexT.motion.middleWares.header).add(NexT.motion.middleWares.postList).add(NexT.motion.middleWares.sidebar).add(NexT.motion.middleWares.footer).bootstrap(),NexT.utils.updateSidebarPosition()},document.addEventListener("DOMContentLoaded",()=>{NexT.boot.registerEvents(),NexT.boot.refresh(),NexT.boot.motion()});;
 /* global CONFIG */
-document.addEventListener("DOMContentLoaded",()=>{"use strict";const e=()=>{localStorage.setItem("bookmark"+location.pathname,window.scrollY)},t=()=>{let e=localStorage.getItem("bookmark"+location.pathname);e=parseInt(e,10),
-// If the page opens with a specific hash, just jump out
-isNaN(e)||""!==location.hash||
-// Auto scroll to the position
-window.anime({targets:document.scrollingElement,duration:200,easing:"linear",scrollTop:e})};!function(o){
-// Create a link element
-const n=document.querySelector(".book-mark-link");
-// Scroll event
-window.addEventListener("scroll",()=>n.classList.toggle("book-mark-link-fixed",0===window.scrollY)),
-// Register beforeunload event when the trigger is auto
-"auto"===o&&(
-// Register beforeunload event
-window.addEventListener("beforeunload",e),document.addEventListener("pjax:send",e)),
-// Save the position by clicking the icon
-n.addEventListener("click",()=>{e(),window.anime({targets:n,duration:200,easing:"linear",top:-30,complete:()=>{setTimeout(()=>{n.style.top=""},400)}})}),t(),document.addEventListener("pjax:success",t)}(CONFIG.bookmark.save)});;
-/* global CONFIG */
 document.addEventListener("DOMContentLoaded",()=>{if(!CONFIG.path)
 // Search DB path
 return void console.warn("`hexo-generator-searchdb` plugin is not installed!");
